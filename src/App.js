4c9932d8aec221c0de7bb4394071d3accd7b01bf
yurim-ai <yurim.lee@dshare.co.kr>
2023-09-25 20:30:37 +0900

    Count 만들기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import Hello from './Hello';
-import './App.css';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24, // 기본 단위 px
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-  }
+  
   return (
     <>
-      {/* 주석 */}
-      <Wrapper>
-        <Hello color= "red" />
-        <Hello color= "pink" name = "dd" isSpecial />
-        <div style={style}>{name}</div>
-        <div className='gray-box'></div>
-      </Wrapper>  
+      <Counter />
     </>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Counter() {
+    // const [number, setNumber] = useState(0);
+    const numberstate = useState(0);
+    // [0 , setter]
+    const number = numberstate[0];
+    const setNumber = numberstate[1];
+
+    const onIncrease = () => {
+        setNumber(number + 1);
+    }
+
+    const onDecrease = () => {
+        setNumber(number - 1);
+    }
   return (
     <div>
-      <h1>0</h1>
-      <button>+1</button>
-      <button>-1</button>
+      <h1>{number}</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
     </div>
   );
 }
