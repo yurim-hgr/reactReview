8390d796188ece1c99b963ab181b429105abad7f
yurim-ai <yurim.lee@dshare.co.kr>
2023-09-25 19:49:39 +0900

    리액트 환경설정 및 기본개념

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import Hello from './Hello';
 import './App.css';
+import Wrapper from './Wrapper';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24, // 기본 단위 px
+    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* 주석 */}
+      <Wrapper>
+        <Hello color= "red" />
+        <Hello color= "pink" name = "dd" isSpecial />
+        <div style={style}>{name}</div>
+        <div className='gray-box'></div>
+      </Wrapper>  
+    </>
   );
 }
 
