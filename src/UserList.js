3915556b82bde6ab46c4d445d58d31ca5478b68e
yurim-ai <yurim.lee@dshare.co.kr>
2023-09-26 19:38:10 +0900

    배열에 항목 제거하기 작업중

--- src/UserList.js
+++ src/UserList.js
@@ -3,10 +3,11 @@ import React from "react";
 // 한 파일에 여러개의 컴포넌트를 선언해도 괜찮습니다.
 
 
-function User({ user }){
+function User({ user, onRemove }){
     return (
         <div>
             <b>{user.username}</b> <span>({user.email})</span>
+            <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     )
 }
@@ -14,13 +15,13 @@ function User({ user }){
 // 비효율적으로 업데이트 하는 것이 아니라, 
 // 수정되지 않는 기존의 값은 그대로 두고 원하는 곳에 내용을 삽입하거나 삭제합니다.
 
-function UserList({users}) {
+function UserList({users , onRemove}) {
     
     return (
         <div>
             {
                 users.map((user, index) => (
-                    <User user = {user} key={index}/>
+                    <User user = {user} key={index} onRemove={onRemove}/>
                 ))
             }
         </div>
