2a60b7fe9c0cd9c3d46d0cdbca442db9d97911b8
yurim-ai <yurim.lee@dshare.co.kr>
2023-09-26 12:07:09 +0900

    useRef 로 특정 DOM 선택하기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef } from 'react';
 
 function InputSample() {
     const [inputs, setInputs ] = useState({
@@ -6,6 +6,7 @@ function InputSample() {
         nickname : ''
     });
 
+    const nameInput = useRef();
     const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
     
     //불변성을 지켜주어야만 리액트 컴포넌트에서 상태가 업데이트가 됐음을 감지 할 수 있고 이에 따라 필요한 리렌더링이 진행됩니다.
@@ -17,8 +18,8 @@ function InputSample() {
        // e.target.name = name , nickname
        
        setInputs({
-        ...inputs,
-        [name] : value
+        ...inputs,// 기존의 input 객체를 복사한 뒤
+        [name] : value // name 키를 가진 값을 value 로 설정
        })
 
     }
@@ -28,14 +29,17 @@ function InputSample() {
             name: '',
             nickname : ''
         })
+        nameInput.current.focus();
     }
 
-    
+    // useRef() 를 사용하여 Ref 객체를 만들고, 
+    // 이 객체를 우리가 선택하고 싶은 DOM 에 ref 값으로 설정해주어야 합니다. 
+    // 그러면,  Ref 객체의 .current 값은 우리가 원하는 DOM 을 가르키게 됩니다.
     return ( 
         <div>
-        <input name="name"  placeholder='이름' value={name} onChange={onChange}/>
+        <input name="name"  placeholder='이름' value={name} onChange={onChange} ref={nameInput}/>
         <input name="nickname" placeholder='닉네임' value={nickname} onChange={onChange} />
-        <button onClick={onReset}>초기화</button>
+        <button onClick={onReset} >초기화</button>
         <div>
             <b>값: </b>
             {name} ({nickname})
