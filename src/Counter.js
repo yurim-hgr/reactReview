afdf35b1632f9071b98068bbb0c28606aa07e4b7
yurim-hgr <dbfla4618@naver.com>
2023-10-03 15:32:29 +0900

    useReducer 사용하여 상태 업데이트 로직 분리하기

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,50 @@
-import React, {useState} from 'react';
+import React, { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
 
 function Counter() {
-    // const [number, setNumber] = useState(0);
-    const numberstate = useState(0);
-    // [0 , setter]
-    const number = numberstate[0];
-    const setNumber = numberstate[1];
+  // 1. useState 로 하기
+  // const [number, setNumber] = useState(0);
 
+  // 2. useState 풀어써보기
+  // const numberstate = useState(0);
+  // const number = numberstate[0];
+  // const setNumber = numberstate[1];
 
-    //그 다음 상태를 파라미터로 넣어준것이 아니라, 값을 업데이트 하는 함수를 파라미터로 넣어주었습니다.
-    // 함수형 업데이트는 주로 나중에 컴포넌트를 최적화를 하게 될 때 사용하게 됩니다.
+  // 3. useReducer써보깅
 
-    const onIncrease = () => {
-        setNumber(preNumber => preNumber + 1);
-    }
+  const [number, dispatch] = useReducer(reducer, 0);
 
-    const onDecrease = () => {
-        setNumber(preNumber => preNumber - 1);
-    }
+  //그 다음 상태를 파라미터로 넣어준것이 아니라, 값을 업데이트 하는 함수를 파라미터로 넣어주었습니다.
+  // 함수형 업데이트는 주로 나중에 컴포넌트를 최적화를 하게 될 때 사용하게 됩니다.
+
+  // 1. useState 로 상태를 관리할때
+  // const onIncrease = () => {
+  //   setNumber((preNumber) => preNumber + 1);
+  // };
+
+  // const onDecrease = () => {
+  //   setNumber((preNumber) => preNumber - 1);
+  // };
+
+  //2. useReducer로 관리할때
+  const onIncrease = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const onDecrease = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
   return (
     <div>
       <h1>{number}</h1>
